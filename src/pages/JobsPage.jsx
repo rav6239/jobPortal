b78688187d8990fb5a4c2e1ad051611cab0f38cf
ravi6239 <ravishanker52984@gmail.com>
2025-03-18 00:04:57 +0530

    Remove unused React import from JobsPage

--- src/pages/JobsPage.jsx
+++ src/pages/JobsPage.jsx
@@ -1,4 +1,3 @@
-import React from 'react'
 import { FaArrowLeft, FaMapMarker } from 'react-icons/fa';
 import { useLoaderData,useNavigate } from 'react-router-dom';
 import {toast} from 'react-toastify';
