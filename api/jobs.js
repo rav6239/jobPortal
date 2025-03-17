c33699b0de2da607c94d5a0f65b9a47add84c107
ravi6239 <ravishanker52984@gmail.com>
2025-03-18 00:13:16 +0530

    Fix API to handle correct jobs.json structure

--- api/jobs.js
+++ api/jobs.js
@@ -1,43 +1,48 @@
-import jobs from '../src/jobs.json';
+import jobsData from '../../src/jobs.json';
 
 export default function handler(req, res) {
   const { method } = req;
+  let response;
 
   switch (method) {
-    case 'GET':
+    case 'GET': {
       const { _limit } = req.query;
       if (_limit) {
-        const limitedJobs = jobs.slice(0, parseInt(_limit));
-        res.status(200).json(limitedJobs);
+        response = jobsData.jobs.slice(0, parseInt(_limit));
       } else {
-        res.status(200).json(jobs);
+        response = jobsData.jobs;
       }
+      res.status(200).json(response);
       break;
-    case 'POST':
+    }
+    case 'POST': {
       const newJob = req.body;
-      jobs.push({ ...newJob, id: jobs.length + 1 });
+      jobsData.jobs.push({ ...newJob, id: String(jobsData.jobs.length + 1) });
       res.status(201).json(newJob);
       break;
-    case 'PUT':
+    }
+    case 'PUT': {
       const updatedJob = req.body;
-      const index = jobs.findIndex(job => job.id === updatedJob.id);
+      const index = jobsData.jobs.findIndex(job => job.id === updatedJob.id);
       if (index !== -1) {
-        jobs[index] = updatedJob;
+        jobsData.jobs[index] = updatedJob;
         res.status(200).json(updatedJob);
       } else {
         res.status(404).json({ message: 'Job not found' });
       }
       break;
-    case 'DELETE':
+    }
+    case 'DELETE': {
       const { id } = req.query;
-      const jobIndex = jobs.findIndex(job => job.id === parseInt(id));
+      const jobIndex = jobsData.jobs.findIndex(job => job.id === id);
       if (jobIndex !== -1) {
-        jobs.splice(jobIndex, 1);
+        jobsData.jobs.splice(jobIndex, 1);
         res.status(200).json({ message: 'Job deleted' });
       } else {
         res.status(404).json({ message: 'Job not found' });
       }
       break;
+    }
     default:
       res.setHeader('Allow', ['GET', 'POST', 'PUT', 'DELETE']);
       res.status(405).end(`Method ${method} Not Allowed`);
--- api/jobs/[id].js
+++ api/jobs/[id].js
@@ -1,38 +1,41 @@
-import jobs from '../../../src/jobs.json';
+import jobsData from '../../../src/jobs.json';
 
 export default function handler(req, res) {
   const { method } = req;
   const { id } = req.query;
 
-  const job = jobs.find(job => job.id === parseInt(id));
+  const job = jobsData.jobs.find(job => job.id === id);
 
   if (!job) {
     return res.status(404).json({ message: 'Job not found' });
   }
 
   switch (method) {
-    case 'GET':
+    case 'GET': {
       res.status(200).json(job);
       break;
-    case 'PUT':
+    }
+    case 'PUT': {
       const updatedJob = req.body;
-      const index = jobs.findIndex(j => j.id === parseInt(id));
+      const index = jobsData.jobs.findIndex(j => j.id === id);
       if (index !== -1) {
-        jobs[index] = updatedJob;
+        jobsData.jobs[index] = updatedJob;
         res.status(200).json(updatedJob);
       } else {
         res.status(404).json({ message: 'Job not found' });
       }
       break;
-    case 'DELETE':
-      const jobIndex = jobs.findIndex(j => j.id === parseInt(id));
+    }
+    case 'DELETE': {
+      const jobIndex = jobsData.jobs.findIndex(j => j.id === id);
       if (jobIndex !== -1) {
-        jobs.splice(jobIndex, 1);
+        jobsData.jobs.splice(jobIndex, 1);
         res.status(200).json({ message: 'Job deleted' });
       } else {
         res.status(404).json({ message: 'Job not found' });
       }
       break;
+    }
     default:
       res.setHeader('Allow', ['GET', 'PUT', 'DELETE']);
       res.status(405).end(`Method ${method} Not Allowed`);
